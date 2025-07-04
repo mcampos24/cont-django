fe736d298b7c8d7d900d4334d1fe651ddaf1f136
CristhoferAlejandroNeira <cneiraa@unal.edu.co>
2025-07-03 20:03:47 -0500

    Se hace intento de las funciones: falta ponerles valores (en proceso)

--- appdjango/urls.py
+++ appdjango/urls.py
@@ -4,3 +4,35 @@ from . import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
 ]
+from django.contrib import admin
+from .views import mostrar_detectar_idioma
+urlpatterns=[
+    path("admin/", admin.site.urls),
+    path("",mostrar_detectar_idioma)
+]
+from .views import mostrar_analisis
+urlpatterns=[
+    path("admin/",admin.site.urls),
+    path("",mostrar_analisis)
+]
+from .views import mostrar_palabra_mas_repetida
+urlpatterns=[
+    path("admin/",admin.site.urls),
+    path("",mostrar_palabra_mas_repetida)
+]
+from .views import mostrar_frecuencia_porcentual
+urlpatterns=[
+    path("admin/",admin.site.urls),
+    path("", mostrar_frecuencia_porcentual)
+]
+from .views import mostrar_varianza_poblacional
+urlpatterns=[
+    path("admin/",admin.site.urls),
+    path("",mostrar_varianza_poblacional)
+]
+from .views import mostrar_sugerir_sinonimos
+urlpatterns=[
+    path("admin/",admin.site.urls),
+    path("",mostrar_sugerir_sinonimos)
+]
+
--- appdjango/views.py
+++ appdjango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .utils import obtener_texto_desde_request, analizar_texto, detectar_idioma
+from .utils import obtener_texto_desde_request, analizar_texto, detectar_idioma, analizar_texto, palabra_mas_repetida, varianza_poblacional,sugerir_sinonimos
 from .idiomas import mensajes_por_idioma
 
 def inicio(request):
@@ -22,3 +22,27 @@ def inicio(request):
         "mensajes": mensajes,
         "idioma_i": idioma_i
     })
+from .utils import detectar_idioma
+def mostrar_detectar_idioma(request):
+    detectaridioma = detectar_idioma()
+    return render(request,"detectar_idioma.html",{"detectar_idioma":detectaridioma})
+from .utils import analizar_texto
+def mostrar_analisis(request):
+    analisis = analizar_texto()
+    return render(request,"analisis.html",{"analisis":analisis})
+from .utils import palabra_mas_repetida
+def mostrar_palabra_mas_repetida(request):
+    palabramasrepetida = palabra_mas_repetida()
+    return render(request,"palabra_mas_repetida",{"palabra_mas_repetida":palabramasrepetida})
+from .utils import frecuencia_porcentual
+def mostrar_frecuencia_porcentual(request):
+    frecuenciaporcentual = frecuencia_porcentual()
+    return render(request,"frecuencia_porcentual.html",{"frecuencia_porcentual":frecuenciaporcentual})
+from .utils import varianza_poblacional
+def mostrar_varianza_poblacional(request):
+    varianzapoblacional=varianza_poblacional()
+    return render(request,"varianza_poblacional.html",{"varianza_poblacional":varianzapoblacional})
+from .utils import sugerir_sinonimos
+def mostrar_sugerir_sinonimos(request):
+    sugerirsinonimos = sugerir_sinonimos()
+    return render(request,"sugerir_sinonimos.html",{"sugerir_sinonimos":sugerirsinonimos})
